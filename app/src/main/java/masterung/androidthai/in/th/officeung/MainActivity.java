458bb8e21ce373cc32277eab06421dd06f3e849d
masterUNG <phrombutr@gmail.com>
2018-10-02 10:31:19 +0700

    MainFragment Success

--- app/src/main/java/masterung/androidthai/in/th/officeung/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/officeung/MainActivity.java
@@ -5,9 +5,24 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+        }
+
+
+    }   // Main Method
+
+
+}   // Main Class
